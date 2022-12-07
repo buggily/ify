88e48d596fc07cb62b831fa36004ff13f248b88a
Lance Holsten <58127678+buggily@users.noreply.github.com>
2022-12-06 22:12:41 -0600

    reorganize builder extensions

--- builder/convention/src/main/kotlin/com.buggily.ify/AndroidCompose.kt
+++ builder/convention/src/main/kotlin/com.buggily.ify/AndroidCompose.kt
@@ -1,14 +1,14 @@
 package com.buggily.ify
 
-import getLib
-import getLibs
-import getVersion
-import com.android.build.api.dsl.CommonExtension
+import ext.getLib
+import ext.getLibs
+import ext.getVersion
 import org.gradle.api.Project
 import org.gradle.api.artifacts.MinimalExternalModuleDependency
 import org.gradle.api.artifacts.VersionCatalog
 import org.gradle.api.provider.Provider
 import org.gradle.kotlin.dsl.dependencies
+import com.android.build.api.dsl.CommonExtension
 
 internal fun Project.configureAndroidCompose(
     commonExtension: CommonExtension<*, *, *, *>,
