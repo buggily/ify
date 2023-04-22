f80798b15d3955bec8ce5cf1996050509a205ce3
Lance Holsten <58127678+buggily@users.noreply.github.com>
2023-04-21 20:10:06 -0500

    update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,6 @@ plugins {
     alias(libs.plugins.kotlin.serialization) apply false
 
     alias(libs.plugins.hilt) apply false
-    id("com.android.library") version "7.4.2" apply false
+    id("com.android.library") version "8.0.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
 }
--- builder/convention/src/main/kotlin/com.buggily.ify/Kotlin.kt
+++ builder/convention/src/main/kotlin/com.buggily.ify/Kotlin.kt
@@ -3,10 +3,13 @@ package com.buggily.ify
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.jvm.toolchain.JavaLanguageVersion
 
 internal fun Project.configureKotlin(
     javaExtension: JavaPluginExtension,
 ) = with(javaExtension) {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+
+    toolchain { languageVersion.set(JavaLanguageVersion.of(17)) }
 }
